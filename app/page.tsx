cb97893a918f0946a243e4827df136d4c28f5400
Sezgin İbiş (incelemeelemani) <incelemeelemani@gmail.com>
2024-02-07 15:46:19 +0300

    uninstall eslint and prettier

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,10 @@ export default function Page() {
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </div>
-      {/* <div className={styles.shape} /> // CSS Module kullanımı örneği */}
 
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          {/* <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Acme&apos;ye hoşgeldiniz.{" "}</strong> Bu örnek yönetim paneli Vercel tarafından sağlanan <a href="https://nextjs.org/learn/" className="text-blue-500"> Next.js Learn Course</a>, sayfasındaki anlatımdan faydalanılarak oluşturulmuştur.
-          </p> */}
+          <p>Anasayfa</p>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
