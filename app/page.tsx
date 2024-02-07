607e0e042df1887d4b3d423053817f30b7d31d22
Sezgin İbiş (incelemeelemani) <incelemeelemani@gmail.com>
2024-02-07 14:28:30 +0300

    tırnak işareti hatası devam ediyor

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Acme{`'`}ye Hoşgeldiniz.</strong> Bu örnek yönetim paneli Vercel tarafından sağlanan <a href="https://nextjs.org/learn/" className="text-blue-500"> Next.js Learn Course</a>, sayfasındaki anlatımdan faydalanılarak oluşturulmuştur.
+            <strong>Acme Sayfasına Hoşgeldiniz.</strong> Bu örnek yönetim paneli Vercel tarafından sağlanan <a href="https://nextjs.org/learn/" className="text-blue-500"> Next.js Learn Course</a>, sayfasındaki anlatımdan faydalanılarak oluşturulmuştur.
           </p>
           {/* <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"/> */}
           {/* <p className={`${roboto.className} text-xl text-gray-800 md:text-2xl md:leading-normal`}>Bilemiyorum Altan, bilemiyorum.</p> */}
